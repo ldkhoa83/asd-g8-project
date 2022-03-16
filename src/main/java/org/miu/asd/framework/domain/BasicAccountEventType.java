e44c857728fe941bea64cce91bc05f8569b9bb35
MilkaElias <milkaelias23@gmail.com>
2022-03-16 11:49:19 -0500

    Added Interest Enum field.

--- src/main/java/org/miu/asd/framework/domain/BasicAccountEventType.java
+++ src/main/java/org/miu/asd/framework/domain/BasicAccountEventType.java
@@ -18,6 +18,11 @@ public enum BasicAccountEventType implements AccountEventType{
         public String getDescription() {
             return "Withdraw";
         }
-    };
+    },
+    INTEREST{
+        @Override
+        public String getDescription(){ return "Interest";}
+    }
+
 
 }
